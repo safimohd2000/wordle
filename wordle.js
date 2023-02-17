0482568ebdfeef9e6004a3086b65b0caa44e0aa3
safi_mohd_2000 <safi_mohd_2000@yahoo.com>
2023-02-17 10:17:53 -0800

    Refactor word size

--- wordle.js
+++ wordle.js
@@ -3,22 +3,23 @@ var currentword = '';
 var targetword = 'FIGHT';
 var rowcounter = 0;
 var maxrowcounter = 6;
+var supportedWordSize = 5;
 document.querySelectorAll('.keys').forEach(e => {
-     e.id=e.innerText;
+    e.id = e.innerText;
     // console.log(e.id  +""+ e.innerText )
     e.addEventListener('click', function () {
-        
+
         //if it is a backspace key last letter needs to be deleted
         if (this.id == 'BSKP') {
             // need to check if there is anything in currentword
 
             if (currentword == '') {
-                
+
                 return
             }
             if (currentword.length > 0) {
                 currentword = currentword.substring(0, currentword.length - 1)
-                document.querySelectorAll('.items')[(5*rowcounter)+currentword.length].innerText = '';
+                document.querySelectorAll('.items')[(supportedWordSize * rowcounter) + currentword.length].innerText = '';
                 return
             }
         }
@@ -26,13 +27,13 @@ document.querySelectorAll('.keys').forEach(e => {
             if (currentword == targetword) {
                 alert('CONGRATUATION YOU WON')
                 return;
-                
+
             }
-            if (currentword.length < 5) {
+            if (currentword.length < supportedWordSize) {
                 alert('PLEASE COMPLETE THE WORD')
                 return
             }
-            if (currentword.length == 5) {
+            if (currentword.length == supportedWordSize) {
                 // need to check 
                 for (var i = 0; i < currentword.length; i++) {
                     //use indexof to find the indexof current letter(currentword[i]) in the targetword.
@@ -41,23 +42,23 @@ document.querySelectorAll('.keys').forEach(e => {
 
                     if (targetword.indexOf(currentword[i]) >= 0) {
                         if (targetword.indexOf(currentword[i]) == i) {
-                            document.querySelectorAll(".items")[(5*rowcounter)+i].style.backgroundColor = "green"
-                             document.getElementById(currentword[i]).style.backgroundColor="green"
+                            document.querySelectorAll(".items")[(supportedWordSize * rowcounter) + i].style.backgroundColor = "green"
+                            document.getElementById(currentword[i]).style.backgroundColor = "green"
                         }
                         else {
-                            document.querySelectorAll(".items")[(5*rowcounter)+i].style.backgroundColor = "yellow"
-                            document.getElementById(currentword[i]).style.backgroundColor="yellow"
+                            document.querySelectorAll(".items")[(supportedWordSize * rowcounter) + i].style.backgroundColor = "yellow"
+                            document.getElementById(currentword[i]).style.backgroundColor = "yellow"
                         }
                     }
                     else {
-                        document.querySelectorAll(".items")[(5*rowcounter)+i].style.backgroundColor = "grey"
-                        document.getElementById(currentword[i]).style.backgroundColor="grey"
+                        document.querySelectorAll(".items")[(supportedWordSize * rowcounter) + i].style.backgroundColor = "grey"
+                        document.getElementById(currentword[i]).style.backgroundColor = "grey"
                     }
 
                 }
-                currentword="";
+                currentword = "";
                 rowcounter++;
-                if (rowcounter==maxrowcounter){
+                if (rowcounter == maxrowcounter) {
                     alert("You have reached your max limit")
                 }
                 return
@@ -65,8 +66,8 @@ document.querySelectorAll('.keys').forEach(e => {
             }
 
         }
-        if (currentword.length <= 4 ) {
-            document.querySelectorAll('.items')[(5*rowcounter)+currentword.length].innerText = e.innerText
+        if (currentword.length <= supportedWordSize) {
+            document.querySelectorAll('.items')[(supportedWordSize * rowcounter) + currentword.length].innerText = e.innerText
             currentword = currentword + e.innerText;
             //  rowcounter++;
         }
